072167af36ada59d533328d5bdd9892a6e2e96b8
BenjaminPlanterose <bpj_94@hotmail.es>
2022-04-28 14:52:19 +0200

    update

--- src/binokulars.R
+++ src/binokulars.R
@@ -1,112 +1,106 @@
-# Call
-# Rscript --vanilla BinomL.R --regions regions.txt --window.length 200 --offset 100 --epiread.file sd_epiread --plot F
+# Rscript --vanilla $MY_PATH/binokulars.R --target $target_file --epiread_chr_dir $input_dir 
+# --bin_length $bin_length --N_iter $N_iter --seed $SEED --output_dir $output_dir 
+# --Lflank $Lflank --nCores $nCores
+
 
 # Read arguments
 options = commandArgs(trailingOnly = TRUE)
 index_names = startsWith(options, '--')
-if(sum(index_names) != length(index_names)/2) stop('All arguments need to be specified like Rscript --vanilla BinomL.R --regions regions.txt --window.length 200 --offset 100 --epiread.file sd_epiread --plot F')
 arguments = options[!index_names]
 names(arguments) = options[index_names]
-print(arguments)
+target_file = arguments['--target']
+epiread_chr_dir = arguments['--epiread_chr_dir']
+bin_length = as.numeric(arguments['--bin_length'])
+N_iter = as.numeric(arguments['--N_iter'])
+seed = as.integer(arguments['--seed'])
+nCores = as.numeric(arguments['--nCores'])
+Lflank = as.numeric(arguments['--Lflank'])
+res_folder = arguments['--output_dir']
+res_dir = paste(getwd(), res_folder, sep = '/')
 
-# Store arguments as variables
-regions = fread(arguments['--regions'])
-filenames = arguments['--epiread.file']
-window.length = as.numeric(arguments['--window.length'])
-offset = as.numeric(arguments['--offset'])
-plot_indicator = as.logical(arguments['--plot'])
+
+setwd(res_dir)
+capture.output(
+print(paste('target_file = ', target_file, '; epiread_chr_dir = ', epiread_chr_dir, '; bin_length = ', bin_length, 
+            '; N_iter = ', N_iter, '; seed = ', seed, 
+            '; res_dir = ', res_dir, '; nCores = ', nCores, sep = '')), file = 'params.txt')
 
 # Load dependencies
 library(data.table)
 library(parallel)
+library(MASS)
 
-
-# Define functions
-# Binomial likelihood approach for ASM calling
-
-s2c_bin <- function(seq)
+permute <- function(P, L)
 {
-  seq = strsplit(seq, split = '')[[1]]
-  seq[seq == 'C'] = 1
-  seq[seq == 'T'] = 0
-  return(as.integer(seq))
+  M = vapply(1:length(P), function(i) rbinom(n = 1, size = L[i], prob = P[i]), as.integer(1L))
+  -sum(stats::dbinom(x = M, size = L, prob = P, log = T))
 }
 
-
-# Better from old epiread format rather than .bam
-# Single Fragment Epireads 
-# chr19  -  3083513  CCCCCCC        3083495  ATT  3083513  CCCCCCC      3083495  ATT
-# chr19  -  3083545  CCTCCCCCCT     .        .    3083527  CCCCTCCCCCC  3083523  AG
-
-# Reg: chr9:124987720-124990776
-read.location <- function(region)
+permutation_test <- function(X, breaks, N_iter)
 {
-  region = 'chr9:124987720-124990776'
-  Chr = strsplit(region, split = ':')[[1]][1]
-  region = strsplit(region, split = ':')[[1]][2]
-  Start = as.integer(strsplit(region, split = '-')[[1]][1])
-  End = as.integer(strsplit(region, split = '-')[[1]][2])
-  command <- paste("grep ", "^", Chr, " ", file, " | ", "awk ", "'", "$3>", Start, " && ",
-                   "$3<", End, "'", sep = "")
-  fread(cmd = command, sep = "\t")
-}
-
-region_list = mclapply(X = regions, FUN = read.location)
-
-# Window.length + offset
-region = data.frame(V1 = c('chr19', 'chr19'), V2 = c('-', '-'), V3 = c('3083513', '3083545'), V4 = c('CCCCCCC', 'CCTCCCCCCT'), 
-                    V5 = c('3083495', '.'), V6 = c('ATT', '.'), V7 = c('3083513', '3083527'), V8 = c('CCCCCCC', 'CCCCTCCCCCC'),
-                    V9 = c('3083495', '3083523'), V10 = c('ATT', 'AG'))
-setA = sapply(region$V4, s2c_bin)
-setB = sapply(region$V8, s2c_bin)
-
-
-
-# Model 1
-
-deltaL = function(R_list)
-{
-  L_model2(R_list) - L_model1(R_list)
-}
-
-L_model1 = function(R_list)
-{
-  likelihood <- function(seq, p)
+  zero_count = sapply(1:nrow(X), function(x) sum(as.numeric(X[x, c('V3', 'V4', 'V5', 'V6'), ]) == 0))
+  X = X[zero_count != 4,]
+  X$bins = cut(X$V2, breaks = breaks, labels = F)
+  M_fw = aggregate(V3 ~ bins, data = X, FUN = sum)$V3
+  n_fw = aggregate(V3+V4 ~ bins, data = X, FUN = sum)$`V3 + V4`
+  M_rv = aggregate(V5 ~ bins, data = X, FUN = sum)$V5
+  n_rv = aggregate(V5+V6 ~ bins, data = X, FUN = sum)$`V5 + V6`
+  p = (M_fw + M_rv)/(n_fw + n_rv)
+  l_bins = table(X$bins)
+  p_vec = unlist(sapply(1:length(l_bins), function(i) rep(p[i], l_bins[i])))
+  l_fw = X$V3+X$V4
+  l_rv = X$V5+X$V6
+  
+  # Combine Fw and Rv in a single vector
+  L = c(l_fw, l_rv)
+  P = c(p_vec, p_vec)
+  
+  # Compute logL under H0
+  logL_H0 = vapply(1:N_iter, function(i) permute(P, L), numeric(1))
+  
+  # Compute logL on data
+  #logL = permute(P, L) # NULL MODE
+  logL = -sum(stats::dbinom(x = c(X$V3, X$V5), size = L, prob = P, log = T)) # test
+  if(logL == Inf) # Fwd methylation of 1, Rv 11110. Probability 0, likelihood infinity.
   {
-    dbinom(x = sum(seq), size = length(seq), prob = p, log = T)
+    return(NA)
   }
   
-  p = mean(unlist(R_list))
-  sum(sapply(R_list, likelihood, p))
+  # Estimate p-values
+  pval = 1 - mean(logL > logL_H0)
+  if(pval == 0)
+  {
+    params = fitdistr(logL_H0, 'Gamma')
+    pval = pgamma(q = logL, shape = params$estimate['shape'], rate = params$estimate['rate'], lower.tail = F)
+  }
+  return(pval)
 }
 
-L_model2 = function(R_list)
+process_region <- function(input_dir, regions_i, Chr_i, Start_i, End_i, bin_length, N_iter)
 {
-  p = mean(sapply(R_list, function(x) mean(x)> 0.5))
-  n = length(R_list)
-  dbinom(x = n*p, size = n, prob = p, log = T)
+  #print(regions_i)
+  setwd(input_dir)
+  command <- paste("awk ", "'", "$2>=", Start_i, " && ", "$2<", End_i, "' ", Chr_i, sep = "")
+  sub_data = fread(cmd = command, sep = "\t")
+  breaks = seq(Start_i-1, End_i, bin_length)
+  permutation_test(sub_data, breaks, N_iter)
 }
 
 
-set0 = list(c(1,1,1,1), c(1,1), c(1,1,1,1,1,1,1,1,1,1), c(1,1,1))
-set1 = list(c(1,1,1,1), c(1,1), c(1,1,1,1,1,1,1,1,1,0), c(1,1,1))
-set2 = list(c(1,0,1,1,0,0,1,1), c(0,1,1,1,1,0,0,0,1,1), c(1,1,0,0), c(1,0,0,0), c(1,0,0,0,0), c(1,0,0,1,1,0,1,1))
-set3 = list(c(1,1,1,1), c(0,0,0,0,0,0,0), c(1,1,1,1,1,1,1,1,1,0), c(1,1,1), c(0,0,0,0,0), c(1,1,1,1,1,1), c(0,0,0,0))
-set4 = list(c(1,1,1,1,1,1,1), c(0,0,0,0,0,0,0,1), c(1,1,1,1,1,1,1,1,1,0), c(1,1,1,0,1), c(0,0,0,0,0), c(1,1,1,1,1,1), c(0,0,0,0,1,1))
-set5 = list(c(1,0,1,1,0,0,1,1), c(0,1,1,1,1,0,0,0,1,1), c(1,1,0,0), c(1,0,0,0,1,0), c(1,0,0,0,0,1,1), c(1,0,0,1,1,0,1,1))
-set6 = list(c(0,0,0,0), c(0,1), c(0,0,0,0,0,0,0,0,0,0), c(0,1,0))
-set7 = list(c(0,0,0,0), c(0,1), c(0,0,0,0,0,0,0,0,0,0), c(0,1,0), c(0,1,0,0,0,0,0), c(0,0,0,0,0,0,0,1), c(0,0,0,0,0,0,0,1))
-set8 = list(c(0,0,0,0), c(0,0), c(0,0,0,0,0,0,0,0,0,0), c(0,0,0))
-sets = list(set0, set1, set2, set3, set4, set5, set6, set7, set8)
 
-plot(sapply(sets, L_model1), type = 'o')
-plot(sapply(sets, L_model2), type = 'o')
+# Read target_file
+regions = fread(input = target_file, header = F)$V1
+Chr = sapply(strsplit(regions, split = ':'), function(x) x[1])
+pos = sapply(strsplit(regions, split = ':'), function(x) x[2])
+Start = as.integer(sapply(strsplit(pos, split = '-'), function(x) x[1])) - Lflank # Add flanks
+End = as.integer(sapply(strsplit(pos, split = '-'), function(x) x[2])) + Lflank # Add flanks
 
-plot(0:8, -sapply(sets, L_model1), type = 'o')
-lines(0:8, sapply(sets, deltaL), type = 'o', lty = 2)
-
-
-
-
-
-
+# Run statistics
+RNGkind("L'Ecuyer-CMRG")
+set.seed(seed)
+pval_vec = mclapply(1:length(regions), function(i) tryCatch(process_region(epiread_chr_dir, regions[i], Chr[i], 
+                                                                  Start[i], End[i], bin_length, N_iter), error = function(x) NA), 
+                    mc.cores = nCores)
+pval_vec = unlist(pval_vec)
+names(pval_vec) = regions
+write.table(x = pval_vec, file = 'p_values.txt', quote = F, col.names = F, sep = '\t')
